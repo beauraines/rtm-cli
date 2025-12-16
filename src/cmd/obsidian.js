a99092846551c5d8646440cb75949c2ad3bb25b2
Beau Raines <beau.raines@gmail.com>
2025-12-16 12:38:39 -0800

    feat: adds obsidian export command

---
+++ src/cmd/obsidian.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const log = require('../utils/log.js');
+const config = require('../utils/config.js');
+const finish = require('../utils/finish.js');
+const filter = require('../utils/filter');
+const { indexPrompt } = require('../utils/prompt');
+const debug = require('debug')('rtm-cli-obsidian');
+
+let TASKS = [];
+
+/**
+ * This command outputs tasks in Obsidian Tasks markdown syntax
+ * @param args indices
+ * @param env
+ */
+async function action(args, env) {
+  TASKS = [];
+  const user = config.user(user => user);
+
+  let indices;
+  if (args.length < 1) {
+    indices = indexPrompt('Task:');
+  } else {
+    // Support multiple indices array
+    indices = Array.isArray(args[0]) ? args[0] : [args[0]];
+  }
+
+  log.spinner.start('Getting Task(s)');
+  for (const idx of indices) {
+    const filterString = filter();
+    let response = await user.tasks.rtmIndexFetchTask(idx, filterString);
+    if (response.err) {
+      log.spinner.warn(`Task #${idx} not found`);
+      continue;
+    }
+    TASKS.push({ idx, task: response.task });
+  }
+  log.spinner.stop();
+
+  for (const { idx, task } of TASKS) {
+    displayObsidianTask(idx, task);
+  }
+
+  finish();
+}
+
+/**
+ * Format and log a single task in Obsidian Tasks syntax
+ */
+function displayObsidianTask(idx, task) {
+  debug(task);
+  const { name, priority, start, due, completed, tags = [], _list, list_id } = task;
+  const listName = _list && _list.name ? _list.name : list_id;
+  const checkbox = completed ? 'x' : ' ';
+  let line = `- [${checkbox}] ${name}`;
+
+  if (start) {
+    let startISO = new Date(start).toISOString().split('T')[0];
+    line += ` 🛫 ${startISO}`;
+  }
+  if (due) {
+    let dueISO = new Date(due).toISOString().split('T')[0];
+    line += ` 📅 ${dueISO}`;
+  }
+
+const priorityMap = { '1': '🔺', '2': '🔼', '3': '🔽' };
+  if (priority && priorityMap[priority]) {
+    line += ` ${priorityMap[priority]}`;
+  }
+
+  // Add list tag first, then other tags
+  const allTags = [`#${listName}`, ...tags.map(t => `#${t}`)];
+  const tagStr = allTags.map(t => ` ${t}`).join('');
+  line += `${tagStr}`;
+
+  line += ` 🆔 ${idx}`;
+
+  log(line);
+}
+
+module.exports = {
+  command: 'obsidian [indices...]',
+  options: [],
+  description: 'Output tasks in Obsidian Tasks syntax',
+  action: action
+};
